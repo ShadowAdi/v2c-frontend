618ab4fb15ca6babc9fd2e64254df0c09f2edd70
ShadowAdi <shadowshukla76@gmail.com>
2026-02-03 02:28:36 +0530

    added chat input

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 'use client'
 import { useSocket } from "@/context/socketContext";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const [input, setInput] = useState("")
   const { socket } = useSocket()
+  const [messages, setMessages] = useState<string[]>([])
 
   const handleSendMessage = (e: any, message: string) => {
     e.preventDefault();
@@ -15,11 +16,28 @@ export default function Home() {
     }
     setInput("");
   };
+
+  useEffect(() => {
+
+    socket?.on("chat-message", (data: string) => {
+      setMessages((prev) => [...prev, data])
+    })
+
+  }, [])
+
   return (
     <main className="flex flex-col items-center w-screen min-h-screen">
       <h1 className="text-2xl font-semibold">
         Chat app
       </h1>
+
+      {messages.map((m)=>{
+        return (
+          <p>
+            {m}
+          </p>
+        )
+      })}
 
       <input type="text" className="w-full h-8 p-2 transition-all bg-gray-100 rounded-full focus:outline-none"
         placeholder="Aa" value={input} onChange={(e) => {
