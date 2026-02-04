02935c6cc3388bf38bb5c09a66d795c27258252d
ShadowAdi <shadowshukla76@gmail.com>
2026-02-05 01:55:42 +0530

    added states

--- app/meet/[meetId]/page.tsx
+++ app/meet/[meetId]/page.tsx
@@ -1,18 +1,30 @@
 'use client'
 import { useSocket } from '@/context/socketContext'
-import { useEffect } from 'react'
+import { use, useEffect, useRef, useState } from 'react'
 import { useParams } from 'next/navigation'
+import Peer from 'peerjs'
 
 const Page = () => {
   const { socket } = useSocket()
-  const params = useParams()
-  const roomId = params.meetId 
-  const newId=crypto.randomUUID()
+  const [isConnected, setIsConnected] = useState(false)
+  const [myUniqueId, setMyUniqueId] = useState<string>("")
+  const [idToConnect, setIdToConnect] = useState<string>("")
+  const { meetId: roomId }: { meetId: string } = useParams()
+
+
+  const localVideoRef = useRef<HTMLVideoElement | null>(null)
+  const remoteVideoRef = useRef<HTMLVideoElement>(null);
+
+  const remoeteStreamRef = useRef<MediaStream | null>(null)
+  const localStreamRef = useRef<MediaStream | null>(null)
+  const peerRef = useRef<Peer | null>(null);
+  const callRef = useRef<any>(null);
+
   useEffect(() => {
     if (!socket || !roomId) return
 
     socket.emit("join-room", {
-      meetId: newId,   // identity of this user
+      meetId: myUniqueId,   // identity of this user
       roomId: roomId       // shared room
     })
 
